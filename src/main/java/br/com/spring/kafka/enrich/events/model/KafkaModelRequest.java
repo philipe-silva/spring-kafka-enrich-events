80408604135abf4df11e51edd20463d05c489045
philipe-silva <philipepereiradasilva@hotmail.com>
2022-07-13 21:56:18 -0300

    Commit Inicial

---
+++ src/main/java/br/com/spring/kafka/enrich/events/model/KafkaModelRequest.java
@@ -0,0 +1,13 @@
+package br.com.spring.kafka.enrich.events.model;
+
+import lombok.Data;
+
+@Data
+public class KafkaModelRequest {
+
+    protected String nome;
+
+    protected String type;
+
+
+}
